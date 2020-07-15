2fa1168fae7835d3df04b91f31706ee408973f53
TXBIT <shenlong203@gmail.com>
2020-07-15 23:52:22 +0700

    create orders

--- server.js
+++ server.js
@@ -92,6 +92,11 @@ app.delete('/api/products/:id', async (req, res) => {
   res.send(deleteProduct);
 });
 
+app.get('/api/orders', async (req, res) => {
+  const orders = await Order.find({});
+  res.send(orders);
+});
+
 app.post('/api/orders', async (req, res) => {
   if (
     !req.body.name ||
@@ -104,6 +109,11 @@ app.post('/api/orders', async (req, res) => {
   }
 
   const order = await Order(req.body).save();
+  res.send(order);
+});
+
+app.delete('/api/orders/:id', async (req, res) => {
+  const order = await Order.findByIdAndDelete(req.params.id);
   res.send(order);
 });
 
