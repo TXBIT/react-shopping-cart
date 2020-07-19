87ff7252fdc5f8c71afd67d919cb75fa9d64a0ba
TXBIT <shenlong203@gmail.com>
2020-07-19 22:44:15 +0700

    deploy to heroku 2

--- src/actions/productActions.js
+++ src/actions/productActions.js
@@ -5,8 +5,7 @@ import {
 } from '../types';
 
 export const fetchProducts = () => async (dispatch) => {
-  const res = await fetch(`http://localhost:5000/api/products`);
-  // const res = await fetch(`/api/products`);
+  const res = await fetch(`/api/products`);
   const data = await res.json();
   dispatch({
     type: FETCH_PRODUCTS,
